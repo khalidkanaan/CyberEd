9fca13a60d78c92a2896a5525eb5bb856032cde0
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2023-10-25 18:10:16 -0400

    added the endpoint to get all the users registered in the application

---
+++ backend/src/contollers/users.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getUsers } from '../db/users';
+
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
+    try {
+        const users = await getUsers();
+        return res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import {get, merge} from 'lodash';
 
-import { getUserBySessionToken } from 'db/users';
+import { getUserBySessionToken } from  '../db/users';
 
 export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
     try{

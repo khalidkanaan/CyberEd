4ea91d1df98b84545b25992fd5735a6c3f9e3540
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2023-10-25 18:38:15 -0400

    added endpoint to delete a user, created isOwner middlewear to ensure only the logged in user is able to delete their account

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -3,6 +3,26 @@ import {get, merge} from 'lodash';
 
 import { getUserBySessionToken } from  '../db/users';
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try{
+        const { id } = req.params;
+        const currentUserId = get (req, 'identity._id') as string;
+
+        if (!currentUserId){
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() != id) {
+            return res.sendStatus(403);
+        }
+        next();
+        
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
     try{
         const sessionToken = req.cookies['TEST-AUTH'];
--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -1,6 +1,8 @@
 import express from 'express'
-import { getAllUsers } from '../contollers/users'
+import { deleteUser, getAllUsers } from '../contollers/users'
+import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers); // adding isAthenticated here allows us to see the users only if a user is logged in
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
 }

736771ff24685429b198a45338c4a473e2666d5d
Mohamed Abdalla <78317393+Mohman556@users.noreply.github.com>
2023-12-22 18:08:36 -0500

    Improved on the formatting of the homepage by adding a header bar. The Header bar includes a stylized login button. After a succesful login, this button changes to a profile button -- this button contains a dropdown with two options: Profile -- does nothing for now and shows username, Logout -- Logs the user out removes all info and cookies

--- frontend/src/pages/BasePage.js
+++ frontend/src/pages/BasePage.js
@@ -1,22 +1,46 @@
-import React from 'react';
+// BasePage.js
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/BasePage.css';
+import { isCookiePresent, handleLogout } from '../assets/js/helpers.js'; // Add handleLogout here
+import logo from '../assets/img/logo-white.png';
+import profile from '../assets/img/drinking.gif';
+import Cookies from 'js-cookie';
 
-class BasePage extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Link to="/login">
+function BasePage(props) {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
+
+  const isLoggedIn = isCookiePresent('TEST-AUTH');
+  const username = Cookies.get('username');
+
+  return (
+    <div>
+      <div className="header">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo"/>
+        </Link>
+        {!isLoggedIn ? (
+          <Link to="/login" className="login-button">
             <button>Login</button>
           </Link>
-        </div>
-        <div>
-          {this.props.children}
-        </div>
+        ) : (
+          <div className="dropdown">
+            <img src={profile} alt="User" className="user-icon" onClick={toggleDropdown}/>
+            {isDropdownOpen && (
+              <div className="dropdown-content">
+                <Link to="/profile">Profile - {username}</Link>
+                <Link to="/" onClick={handleLogout}>Logout</Link>
+              </div>
+            )}
+          </div>
+        )}
       </div>
-    );
-  }
+      <div>
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
 export default BasePage;

1368415846eaf7911f66324ed99195de7c866ea9
Zeid Alwash <zeid.alwash@gmail.com>
2023-12-22 19:48:14 -0500

    Implemented the password policy for the backend

--- backend/src/contollers/authentication.ts
+++ backend/src/contollers/authentication.ts
@@ -41,15 +41,37 @@ export const login = async (req: express.Request, res: express.Response) => {
 export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { username, password, email } = req.body;
+        let errors = [];
 
         if (!username || !password || !email ) {
-            return res.sendStatus(400);
+            errors.push('Please provide a username, password and email.');
+        }
+
+        // Validate the password
+        if (password.length < 9 || password.length > 100) {
+            errors.push('Password length should be more than 9 characters long.');
+        }
+
+        if (!/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[\W_]/.test(password)) {
+            errors.push('Password should include uppercase and lowercase letters, numbers, and special characters.');
+        }
+
+        if (password === username) {
+            errors.push('Password should not be the same as the username.');
+        }
+
+        if (password.toLowerCase().includes(username.toLowerCase())) {
+            errors.push('Password should not contain the username.');
         }
 
         const existingUser = await getUserByEmail(email);
 
         if (existingUser) {
-            return res.sendStatus(400);
+            errors.push('This username is taken.');
+        }
+
+        if (errors.length > 0) {
+            return res.status(400).json(errors);
         }
 
         const salt = random();

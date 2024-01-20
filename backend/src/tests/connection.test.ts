053b96983628f3df58f6970d63c8d4fd9e7fe83f
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2024-01-19 19:29:10 -0500

    added test to check connection to our remote mongo database

--- backend/src/tests/connection.test.ts
+++ backend/src/tests/connection.test.ts
@@ -1,3 +1,19 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+describe('Database Connection', () => {
+  beforeAll(async () => {
+    await mongoose.connect(`${process.env.DB_URI}`);
   });
+
+  it('should be able to connect to the database', async () => {
+    const db = mongoose.connection;
+    expect(db.readyState).toBe(1);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
